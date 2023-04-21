511a81435b5940b813e19a4d701bfe4be729f2ec
Xipho <xipho@mail.ru>
2023-04-21 21:46:38 +0300

    reworked scheduler

--- src/main/kotlin/ru/xipho/godvillebotmodern/job/CheckHeroState.kt
+++ src/main/kotlin/ru/xipho/godvillebotmodern/job/CheckHeroState.kt
@@ -1,26 +1,41 @@
 package ru.xipho.godvillebotmodern.job
 
+import org.quartz.*
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.annotation.Scheduled
 import ru.xipho.godvillebotmodern.bot.GodvilleBot
-import java.util.concurrent.Semaphore
-import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableScheduling
-class CheckHeroState(
-    private val bot: GodvilleBot
-) {
+class CheckHeroStateConfig {
 
-    private val isChecking = Semaphore(0)
-
-    @Scheduled(fixedRate = 30000)
-    fun doCheck() {
-        if (isChecking.tryAcquire(1, TimeUnit.SECONDS)) {
-            bot.run()
-            isChecking.release()
-        }
+    @Bean
+    fun jobDetail(): JobDetail {
+        return JobBuilder.newJob(CheckHeroState::class.java)
+            .withIdentity("checkHeroState")
+            .storeDurably()
+            .build()
     }
 
+    @Bean
+    fun trigger(jobDetail: JobDetail): Trigger {
+
+        val scheduler = SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(30).repeatForever()
+
+        return TriggerBuilder.newTrigger()
+            .forJob(jobDetail)
+            .withIdentity("checkHeroStateTrigger")
+            .withSchedule(scheduler)
+            .build()
+    }
+}
+
+@DisallowConcurrentExecution
+class CheckHeroState(
+    private val bot: GodvilleBot
+): Job {
+    override fun execute(context: JobExecutionContext?) {
+        bot.run()
+    }
 }
